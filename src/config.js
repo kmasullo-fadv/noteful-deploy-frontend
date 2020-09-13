92d8a233ae1ac51fad2754fe2914df0a43a9bd8e
SoundSlab <administrator@Mac-mini.local>
2020-09-13 10:04:43 -0400

    attempt 1

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   PORT: process.env.PORT || 8000,
-  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000',
-  API_TOKEN: process.env.API_TOKEN,
+  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
+  API_TOKEN: process.env.REACT_APP_API_KEY,
 }
